a33bf012a858486df4e68941f2def7a058c7ed85
Liam Gray <hoxxep@gmail.com>
2014-12-03 14:33:11 +0000

    Updated present to load image etc.

--- src/com/wiringtheworld/slicktiled/level/Level.java
+++ src/com/wiringtheworld/slicktiled/level/Level.java
@@ -3,6 +3,7 @@ package com.wiringtheworld.slicktiled.level;
 import com.wiringtheworld.slicktiled.Game;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -13,11 +14,9 @@ public class Level extends BasicGameState {
     static final Vector G = new Vector(0F,9.81F);
     static final Vector RES = new Vector(0.99F,1F);
 
-    private double helloWorldMillis = 0;
-    private int helloWorldx = 0;
+    static Image santaImage;
 
-    private int santaXPos;
-    private int santaXTime;
+    Present present;
 
     public Level(int state) {
 
@@ -30,20 +29,17 @@ public class Level extends BasicGameState {
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-        santaXPos = 0;
-
+        Present.loadImages();
+        present = new Present(new Vector(0,0), new Vector(0,0));
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-        graphics.drawString("Hello world!", helloWorldx, 400);
+        present.render(graphics);
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-        helloWorldMillis += i;
-        helloWorldx = (int) ((Math.sin(2 * Math.PI * (helloWorldMillis/1000.0)) + 1)*50 + 300);
-        santaXTime += i;
-        santaXPos = santaXTime / 100;
+
     }
 }
