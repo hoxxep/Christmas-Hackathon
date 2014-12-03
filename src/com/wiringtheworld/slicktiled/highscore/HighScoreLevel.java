3323a5a20273aa3174c61330122545f01b6b72c4
christoi28 <ci230@cam.ac.uk>
2014-12-03 15:14:41 +0000

    Highscore classes

---
+++ src/com/wiringtheworld/slicktiled/highscore/HighScoreLevel.java
@@ -0,0 +1,45 @@
+package com.wiringtheworld.slicktiled.highscore;
+
+import com.wiringtheworld.slicktiled.Game;
+import com.wiringtheworld.slicktiled.level.Present;
+import com.wiringtheworld.slicktiled.level.Vector;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * Created by chris on 03/12/2014.
+ */
+public class HighScoreLevel extends BasicGameState{
+
+
+
+     public HighScoreLevel(int state) {
+
+        }
+
+    @Override public int getID() {
+            return Game.LEVEL;
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+
+        }
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+
+        }
+    }
+
+
+
