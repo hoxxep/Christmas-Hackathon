f3bd18ef2e2a34752d591b3fc6fda6098ef26243
christoi28 <ci230@cam.ac.uk>
2014-12-03 16:59:13 +0000

    Enter your own name on highscores

--- src/com/wiringtheworld/slicktiled/level/Level.java
+++ src/com/wiringtheworld/slicktiled/level/Level.java
@@ -1,9 +1,11 @@
 package com.wiringtheworld.slicktiled.level;
 
 import com.wiringtheworld.slicktiled.Game;
+import com.wiringtheworld.slicktiled.highscore.Score;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import java.util.Random;
 
 public class Level extends BasicGameState {
 
@@ -20,9 +22,26 @@ public class Level extends BasicGameState {
 
     Image background;
 
+    class Snowflake {
+        public float t, x, p;
+        public Vector getPos(float tnow) {
+            return new Vector((float)(x + Math.sin((tnow - t) % 1000 * p)), (float)((tnow - t)*p%1000));
+        }
+    }
+    Snowflake[] snow;
+
+
     public Level(int state) {
         presentsLeft = DEFAULT_PRESENTS;
         score = 0;
+        snow =  new Snowflake[100];
+        Random r = new Random();
+        for (int i = 0; i < 100; ++i) {
+            snow[i] = new Snowflake();
+            snow[i].t = r.nextFloat() * 100;
+            snow[i].x = r.nextFloat() * Game.WIDTH;
+            snow[i].p = r.nextFloat() * 0.2f + 0.1f;
+        }
     }
 
     @Override
@@ -48,6 +67,8 @@ public class Level extends BasicGameState {
         container.setTargetFrameRate(60); //Display.getDisplayMode().getFrequency());
     }
 
+    private float time = 0;
+
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
         graphics.drawImage(background, 0, 0);
@@ -56,6 +77,11 @@ public class Level extends BasicGameState {
         santa.render(graphics);
         chimney.render(graphics);
         graphics.drawString("Score: " + Integer.toString(score), 100, 10);
+        for (int i = 0; i < 100; ++i) {
+            Vector p = snow[i].getPos(time + 10000);
+            graphics.setColor( org.newdawn.slick.Color.white);
+            graphics.drawRect(p.x, p.y, 1, 1);
+        }
     }
 
     @Override
@@ -81,6 +107,7 @@ public class Level extends BasicGameState {
         }
         chimney.update(i);
         santa.update(i);
+        time += i;
     }
 
     @Override
